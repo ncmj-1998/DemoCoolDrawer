81493a5db019e1b374f56f9db771fbbf09b87c58
16022671 <16022671@myrp.edu.sg>
2018-08-07 10:48:12 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/webservices/democooldrawer/AnniversaryFragment.java
@@ -0,0 +1,27 @@
+package sg.edu.rp.webservices.democooldrawer;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class AnniversaryFragment extends Fragment {
+
+    public AnniversaryFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_anniversary, container, false);
+    }
+
+}
---
+++ app/src/main/java/sg/edu/rp/webservices/democooldrawer/VaccinationFragment.java
@@ -0,0 +1,27 @@
+package sg.edu.rp.webservices.democooldrawer;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class VaccinationFragment extends Fragment {
+
+    public VaccinationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vaccination, container, false);
+    }
+
+}
